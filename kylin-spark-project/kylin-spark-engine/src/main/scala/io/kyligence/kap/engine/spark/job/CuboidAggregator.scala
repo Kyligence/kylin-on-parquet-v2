d6f7ee21de998dd5966c5e6d3d89a68295bcc70c
rupengwang <wangrupeng@live.cn>
2019-12-12 17:51:21 +0800

    resolve conficts

--- kylin-spark-project/kylin-spark-engine/src/main/scala/io/kyligence/kap/engine/spark/job/CuboidAggregator.scala
+++ kylin-spark-project/kylin-spark-engine/src/main/scala/io/kyligence/kap/engine/spark/job/CuboidAggregator.scala
@@ -19,6 +19,7 @@
 package io.kyligence.kap.engine.spark.job
 
 import java.util
+import java.util.Locale
 
 import io.kyligence.kap.engine.spark.builder.DFBuilderHelper.ENCODE_SUFFIX
 import org.apache.kylin.engine.spark.metadata.cube.model.{CubeJoinedFlatTableDesc, DataSegment, MeasureDesc, SpanningTree}
@@ -91,7 +92,7 @@ object CuboidAggregator {
         }
       }
 
-      expression.toUpperCase match {
+      expression.toUpperCase(Locale.ROOT) match {
         case "MAX" =>
           max(columns.head).as(e._1.toString)
         case "MIN" =>
@@ -181,7 +182,7 @@ object CuboidAggregator {
 
   private def measureColumns(schema: StructType, measures: util.Map[Integer, MeasureDesc]): mutable.Iterable[Column] = {
     measures.asScala.map { e =>
-      e._2.getFunction.getExpression.toUpperCase match {
+      e._2.getFunction.getExpression.toUpperCase(Locale.ROOT) match {
         case "SUM" =>
           val measureId = e._1.toString
           val dataType = schema.find(_.name.equals(measureId)).map(_.dataType).get
