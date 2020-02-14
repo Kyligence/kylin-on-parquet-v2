85ba21d8c7f0b0a7cde03330188dc02971a45be4
rupengwang <wangrupeng@live.cn>
2020-02-14 15:33:57 +0800

    merge segments for parquet storage build engine

--- kylin-spark-project/kylin-spark-engine/src/main/java/io/kyligence/kap/engine/spark/merger/AfterMergeOrRefreshResourceMerger.java
+++ kylin-spark-project/kylin-spark-engine/src/main/java/io/kyligence/kap/engine/spark/merger/AfterMergeOrRefreshResourceMerger.java
@@ -24,93 +24,111 @@
 
 package io.kyligence.kap.engine.spark.merger;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.kylin.common.KylinConfig;
 import org.apache.kylin.common.persistence.ResourceStore;
+import org.apache.kylin.cube.CubeInstance;
+import org.apache.kylin.cube.CubeManager;
+import org.apache.kylin.cube.CubeSegment;
+import org.apache.kylin.cube.CubeUpdate;
 import org.apache.kylin.job.execution.AbstractExecutable;
 import org.apache.kylin.job.execution.JobTypeEnum;
+import org.apache.kylin.metadata.MetadataConstants;
 import org.apache.kylin.metadata.model.SegmentStatusEnum;
 
 import com.clearspring.analytics.util.Lists;
+import org.apache.kylin.metadata.model.Segments;
 
-import io.kyligence.kap.engine.spark.ExecutableUtils;
-import io.kyligence.kap.metadata.cube.model.NDataLayout;
-import io.kyligence.kap.metadata.cube.model.NDataSegment;
-import io.kyligence.kap.metadata.cube.model.NDataflow;
-import io.kyligence.kap.metadata.cube.model.NDataflowManager;
-import io.kyligence.kap.metadata.cube.model.NDataflowUpdate;
-import lombok.val;
+public class AfterMergeOrRefreshResourceMerger extends MetadataMerger {
 
-public class AfterMergeOrRefreshResourceMerger extends SparkJobMetadataMerger {
-
-    public AfterMergeOrRefreshResourceMerger(KylinConfig config, String project) {
-        super(config, project);
+    public AfterMergeOrRefreshResourceMerger(KylinConfig config) {
+        super(config);
     }
 
     @Override
-    public NDataLayout[] merge(String dataflowId, Set<String> segmentIds, Set<Long> layoutIds,
-            ResourceStore remoteResourceStore, JobTypeEnum jobType) {
+    public void merge(String cubeId, Set<String> segmentIds, ResourceStore remoteResourceStore, String jobType) {
 
-        NDataflowManager mgr = NDataflowManager.getInstance(getConfig(), getProject());
-        NDataflowUpdate update = new NDataflowUpdate(dataflowId);
+        CubeManager cubeManager = CubeManager.getInstance(getConfig());
+        CubeInstance cubeInstance = cubeManager.getCubeByUuid(cubeId);
+        CubeUpdate update = new CubeUpdate(cubeInstance.latestCopyForWrite());
 
-        NDataflowManager distMgr = NDataflowManager.getInstance(remoteResourceStore.getConfig(), getProject());
-        NDataflow distDataflow = distMgr.getDataflow(update.getDataflowId()).copy(); // avoid changing cached objects
+        CubeManager distManager = CubeManager.getInstance(remoteResourceStore.getConfig());
+        CubeInstance distCube = distManager.getCubeByUuid(cubeId).latestCopyForWrite();
 
-        List<NDataSegment> toUpdateSegments = Lists.newArrayList();
-        List<NDataLayout> toUpdateCuboids = Lists.newArrayList();
+        List<CubeSegment> toUpdateSegments = Lists.newArrayList();
 
-        NDataSegment mergedSegment = distDataflow.getSegment(segmentIds.iterator().next());
-
-        if (mergedSegment.getStatus() == SegmentStatusEnum.NEW)
-            mergedSegment.setStatus(SegmentStatusEnum.READY);
+        CubeSegment mergedSegment = distCube.getSegmentById(segmentIds.iterator().next());
+        mergedSegment.setStatus(SegmentStatusEnum.READY);
 
         toUpdateSegments.add(mergedSegment);
-        if (JobTypeEnum.INDEX_REFRESH.equals(jobType)) {
-            updateSnapshotTableIfNeed(mergedSegment);
+        if (String.valueOf(JobTypeEnum.INDEX_REFRESH).equals(jobType)) {
+            //TODO: update snapshot
+            //updateSnapshotTableIfNeed(mergedSegment);
         }
-        // only add layouts which still in segments, others maybe deleted by user
-        List<NDataSegment> toRemoveSegments = distMgr.getToRemoveSegs(distDataflow, mergedSegment);
-        if (JobTypeEnum.INDEX_MERGE.equals(jobType)) {
+
+        List<CubeSegment> toRemoveSegments = getToRemoveSegs(distCube, mergedSegment);
+        if (String.valueOf(JobTypeEnum.INDEX_MERGE).equals(jobType)) {
             Optional<Long> reduce = toRemoveSegments.stream()
-                    .map(NDataSegment::getSourceBytesSize)
+                    .map(CubeSegment::getSizeKB)
                     .filter(size -> size != -1)
                     .reduce(Long::sum);
             if (reduce.isPresent()) {
                 long totalSourceSize = reduce.get();
-                mergedSegment.setSourceBytesSize(totalSourceSize);
+                mergedSegment.setSizeKB(totalSourceSize);
                 mergedSegment.setLastBuildTime(System.currentTimeMillis());
             }
         }
-        val livedLayouts = mgr.getDataflow(dataflowId).getLatestReadySegment().getLayoutsMap().values().stream()
-                .map(NDataLayout::getLayoutId).collect(Collectors.toSet());
-        toUpdateCuboids.addAll(mergedSegment.getSegDetails().getLayouts().stream()
-                .filter(c -> livedLayouts.contains(c.getLayoutId())).collect(Collectors.toList()));
 
-        update.setToAddOrUpdateLayouts(toUpdateCuboids.toArray(new NDataLayout[0]));
-        update.setToRemoveSegs(toRemoveSegments.toArray(new NDataSegment[0]));
-        update.setToUpdateSegs(toUpdateSegments.toArray(new NDataSegment[0]));
+        update.setToRemoveSegs(toRemoveSegments.toArray(new CubeSegment[0]));
+        update.setToUpdateSegs(toUpdateSegments.toArray(new CubeSegment[0]));
 
-        mgr.updateDataflow(update);
+        try {
+            cubeManager.updateCube(update);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        return update.getToAddOrUpdateLayouts();
+    }
+
+    List<CubeSegment> getToRemoveSegs(CubeInstance cube, CubeSegment mergedSegment) {
+        Segments tobe = cube.calculateToBeSegments(mergedSegment);
+
+        if (!tobe.contains(mergedSegment))
+            throw new IllegalStateException(
+                    "For Cube " + cube + ", segment " + mergedSegment + " is expected but not in the tobe " + tobe);
+
+        if (mergedSegment.getStatus() == SegmentStatusEnum.NEW)
+            mergedSegment.setStatus(SegmentStatusEnum.READY);
+
+        List<CubeSegment> toRemoveSegs = Lists.newArrayList();
+        for (CubeSegment s : cube.getSegments()) {
+            if (!tobe.contains(s))
+                toRemoveSegs.add(s);
+        }
+
+        return toRemoveSegs;
     }
 
     @Override
     public void merge(AbstractExecutable abstractExecutable) {
-        try (val buildResourceStore = ExecutableUtils.getRemoteStore(this.getConfig(), abstractExecutable)) {
-            val dataFlowId = ExecutableUtils.getDataflowId(abstractExecutable);
-            val segmentIds = ExecutableUtils.getSegmentIds(abstractExecutable);
-            val layoutIds = ExecutableUtils.getLayoutIds(abstractExecutable);
-            NDataLayout[] nDataLayouts = merge(dataFlowId, segmentIds, layoutIds, buildResourceStore,
-                    abstractExecutable.getJobType());
-            recordDownJobStats(abstractExecutable, nDataLayouts);
-            abstractExecutable.notifyUserIfNecessary(nDataLayouts);
-        }
+        String buildStepUrl = abstractExecutable.getParam(MetadataConstants.P_OUTPUT_META_URL);
+        KylinConfig buildConfig = KylinConfig.createKylinConfig(this.getConfig());
+        buildConfig.setMetadataUrl(buildStepUrl);
+        ResourceStore resourceStore = ResourceStore.getStore(buildConfig);
+        String cubeId = abstractExecutable.getParam(MetadataConstants.P_CUBE_ID);
+        Set<String> segmentIds = Stream.of(StringUtils.split(abstractExecutable.getParam(MetadataConstants.P_SEGMENT_IDS), ","))
+                .collect(Collectors.toSet());
+        merge(cubeId, segmentIds, resourceStore, abstractExecutable.getParam(MetadataConstants.P_JOB_TYPE));
+        //recordDownJobStats(abstractExecutable, nDataLayouts);
+        //abstractExecutable.notifyUserIfNecessary(nDataLayouts);
+
     }
 
 }
