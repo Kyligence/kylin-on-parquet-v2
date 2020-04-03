8c0287647ef5e8cbf30e3b2677e32253d4afdc90
rupengwang <wangrupeng@live.cn>
2020-04-03 19:29:56 +0800

    #145 Add unit test cases for spark cubing engine

---
+++ kylin-spark-project/kylin-spark-common/src/test/java/org/apache/spark/sql/common/LocalMetadata.scala
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.common
+
+import java.io.File
+
+import org.apache.kylin.common.util.LocalFileMetadataTestCase
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Suite}
+
+trait LocalMetadata extends BeforeAndAfterAll with BeforeAndAfterEach {
+  self: Suite =>
+  val metadata = "../examples/test_case_data/parquet_test"
+  var metaStore: LocalFileMetadataTestCase = _
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    metaStore = new LocalFileMetadataTestCase
+    if (new File(metadata).exists()) {
+      metaStore.createTestMetadata(metadata)
+    } else {
+      metaStore.createTestMetadata("../" + metadata)
+    }
+  }
+
+
+  override def afterAll() {
+    super.afterAll()
+    try {
+      metaStore.cleanupTestMetadata()
+    } catch {
+      case ignore: Exception =>
+    }
+  }
+}
